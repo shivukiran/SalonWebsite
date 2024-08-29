e3c3579b70080a0a72a2dbacead34efaeae7264e
shivukiran <ksvp1125@gmail.com>
2024-08-30 03:26:53 +0530

    added rendering

--- server.js
+++ server.js
@@ -10,19 +10,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname)); 
 // Handle form submission
 app.post('/submit-form', (req, res) => {
-    const { fname, subject, service, message } = req.body;
+    const { fname, subject,mobile, service, message } = req.body;
 
     // Here, you would typically handle the form data,
     // e.g., save it to a database or send an email.
     console.log(`Name: ${fname}`);
     console.log(`Subject: ${subject}`);
+    console.log(`Subject: ${mobile}`);
     console.log(`Service: ${service}`);
     console.log(`Message: ${message}`);
 
     // Send a response to the client
-    res.send('Thank you for contacting us!');
+    // res.send('Thank you for contacting us!');
+    res.render('thankyou', { fname, subject,mobile, service, message });
 });
 
 
